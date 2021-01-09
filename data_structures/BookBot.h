6a351e17e565dfda36820a7881c8f7abbe45c83b
Gage Dimapindan <60364461+GageTheLazy@users.noreply.github.com>
2021-01-08 20:01:36 -0800

    Add files via upload

---
+++ data_structures/BookBot.h
@@ -0,0 +1,19 @@
+#include <fstream>
+#include <vector>
+#include <string>
+#include <map>
+
+#include <iostream>
+
+class BookBot {
+private:
+    std::map<std::string, std::vector<std::string>> markov_chain;
+
+public:
+    BookBot(const unsigned int seed);
+
+    void readIn(const std::string & filename);
+    bool isEndPunctuation(const char character);
+    std::vector<std::string> getValues(const std::string & key);
+    std::string generateSentence();
+};
