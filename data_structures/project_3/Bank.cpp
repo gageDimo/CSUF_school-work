e96a97d8adfa67cdc111ed71609e8aca03b3d6da
Gage Dimapindan <60364461+GageTheLazy@users.noreply.github.com>
2021-01-08 20:00:12 -0800

    Add files via upload

---
+++ data_structures/project_3/Bank.cpp
@@ -0,0 +1,19 @@
+// Michael Dimapindan CPSC 131
+
+#include<string>
+#include "Bank.hpp"
+
+/************************************************
+** Member function definitions
+************************************************/
+// PLEASE ADD IMPLEMENTATIONS OF ALL FUNCTION MEMBERS OF CLASS BANK
+
+  Bank::Bank(const std::string &name, const double &starting,
+              const double &salary) {
+
+        name_ = name;
+        amount_starting_ = starting;
+        amount_salary_ = salary;
+        amount_left_ = 0.0;
+
+    }
---
+++ data_structures/project_3/README.md
@@ -0,0 +1,4 @@
+# Project 3
+## CSUF CPSC 131, Fall 2019
+
+Michael Dimapindan mdimapindan@csu.fullerton.edu
